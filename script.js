321dacefa67edac07c2a43cbb116bab95315a7f5
Koradia Yash <119101794+yk1302@users.noreply.github.com>
2024-03-27 05:06:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var typed = new Typed('.text', {
+    strings: ["Web Developer", "App Developer", , "Java Programmer", "Coder", "GDSC Tech Lead", "Learner"],
+    /**
+     * @property {number} typeSpeed type speed in milliseconds
+     */
+    typeSpeed: 100,
+    /**
+     * @property {number} backSpeed backspacing speed in milliseconds
+     */
+    backSpeed: 100,
+    /**
+     * @property {boolean} smartBackspace only backspace what doesn't match the previous string
+     */
+    smartBackspace: true,
+    /**
+     * @property {number} backDelay time before backspacing in milliseconds
+     */
+    backDelay: 1000,
+    /**
+* @property {boolean} loop loop strings
+
+*/
+    loop: true,
+
+
+});
